31becdf821fcdf87918c328eb02225e0509555f0
Florian Schweizer <post.an.flo@gmail.com>
2022-05-22 09:12:27 +0200

    Initial Commit

---
+++ SwiftUISeries-Animation/ContentView.swift
@@ -0,0 +1,16 @@
+// Created by Florian Schweizer on 22.05.22
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISeries-Animation/SwiftUISeries_AnimationApp.swift
@@ -0,0 +1,12 @@
+// Created by Florian Schweizer on 22.05.22
+
+import SwiftUI
+
+@main
+struct SwiftUISeries_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
